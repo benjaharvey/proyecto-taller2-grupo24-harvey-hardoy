50ee767d0392f5b88903d51cf0e848e181caabb6
benjaharvey <benjaharvey@gmail.com>
2026-09-22 18:27:59 -0300

    subo validaciones al formulario de alta de usuario

--- Presentacion/Vistas/AltaUsuarioView.xaml.cs
+++ Presentacion/Vistas/AltaUsuarioView.xaml.cs
@@ -26,8 +26,23 @@ public partial class AltaUsuarioView : UserControl
         _listarRoles = App.Services.GetRequiredService<ListarRoles>();
         _listarUsuarios = App.Services.GetRequiredService<ListarUsuarios>();
 
+        dtpFechaNacimiento.DisplayDateEnd = DateTime.Today;
+
         CargarComboRoles();
         CargarGrilla();
+    }
+
+    private static bool EsSoloDigitos(string? texto) => !string.IsNullOrEmpty(texto) && texto.All(char.IsAsciiDigit);
+
+    private void SoloDigitos_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+    {
+        e.Handled = !EsSoloDigitos(e.Text);
+    }
+
+    private void SoloDigitos_Pasting(object sender, DataObjectPastingEventArgs e)
+    {
+        if (!e.DataObject.GetDataPresent(typeof(string)) || !EsSoloDigitos((string)e.DataObject.GetData(typeof(string))))
+            e.CancelCommand();
     }
 
     private void CargarComboRoles()
